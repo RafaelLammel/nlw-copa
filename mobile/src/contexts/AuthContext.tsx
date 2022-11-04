11997d027911b2535890f22b84468077155f3522
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2022-11-04 00:41:29 -0300

    Inicio da estilização do projeto mobile

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
+
+interface UserProps {
+  name: string;
+  avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  isUserLoading: boolean;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: "155227754905-jobfk5ojg1uk5mhn0okvsalqntt4lndp.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  });
+
+  async function signIn() {
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  async function signInWithGoogle(accessToken: string) {
+    console.log("Token de Autenticação", accessToken);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  return (
+    <AuthContext.Provider value={{
+      signIn,
+      isUserLoading,
+      user
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
